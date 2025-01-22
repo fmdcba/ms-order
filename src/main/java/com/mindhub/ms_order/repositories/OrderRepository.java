bbfcb7b9c49e8daa0b143e01ee34dffa969d96a2
fmdcba <facudiaz1738@hotmail.com>
2025-01-22 13:10:37 -0300

    renamed repo

--- src/main/java/com/mindhub/ms_order/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mindhub.ms_order.repositories;
-
-import com.mindhub.ms_order.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
